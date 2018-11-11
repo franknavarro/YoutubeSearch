0ab7ff36a4116a7d3f6b9f78d4dbab4e96cec26f
Frank <fnavarro7777@gmail.com>
2018-11-11 10:10:26 -0800

    Implemented grabbing youtube data

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 // Youtube API Key
 const API_KEY = '***REMOVED***';
 
 // Create a new component. This component should produce some html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App  extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    // Grab start videos from youtube
+    YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
