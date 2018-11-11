8c50fc8c413d0409d2b6f438f163a100fb5cb022
Frank <fnavarro7777@gmail.com>
2018-11-11 09:54:49 -0800

    Set up skeleton for search bar

---
+++ src/components/search_bar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    // State is an object that react uses to set values to save within a class
+    this.state = { term: '' };
+  }
+
+  // React default method used for rendering a class even with JSX tags
+  render() {
+    // use this.SetState() to change state not this.state= because this.setState()
+    // informs React that the state has changed
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}
+        />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
