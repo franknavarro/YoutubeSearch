e593534e8b728e02439482126af1ba6096943c24
Frank <fnavarro7777@gmail.com>
2018-11-11 11:18:10 -0800

    Added ability to load videos that are clicked in the list

--- src/index.js
+++ src/index.js
@@ -12,19 +12,28 @@ class App  extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     // Grab start videos from youtube
-    YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
-      this.setState({ videos });
+    YTSearch({key: API_KEY, term: 'super smash bros'}, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
   render() {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
